cd5be07c56a43f5b6b3d70c7e2142dd816aa5cd1
periyana <periyana1746@gmail.com>
2024-10-19 09:18:36 +0700

    portofolio

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/portofolio/',
   server: {
     hmr: {
       overlay: false, // Disable the HMR error overlay
